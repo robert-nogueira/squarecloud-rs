7a14d87134b62c014f0ecccb7cf9beaa237ba9ac
Robert <contact@robertnogueira.com>
2026-02-04 13:28:04 -0300

    ✨ feat(network.rs): add set_custom_domain method to AppResource for custom domain support

--- src/resources/app/network.rs
+++ src/resources/app/network.rs
@@ -1,3 +1,5 @@
+use serde_json::json;
+
 use crate::{
     Endpoint,
     http::errors::ApiError,
@@ -19,5 +21,20 @@ impl AppResource {
             .request_endpoint(Endpoint::get_app_dns_record(&self.id))
             .await?
             .into_result_t()
+    }
+
+    pub async fn set_custom_domain(
+        &self,
+        custom: &str,
+    ) -> Result<bool, ApiError> {
+        let endpoint = Endpoint::set_app_custom_domain(&self.id);
+        let request = endpoint
+            .request_builder(&self.api.http_client)
+            .json(&json!({"custom": custom}))
+            .build()?;
+        self.api
+            .execute_request::<()>(request)
+            .await?
+            .into_bool_result()
     }
 }
