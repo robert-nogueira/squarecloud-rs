4371202a85e864bb2790075ede77ccb7bb61e243
Robert <contact@robertnogueira.com>
2026-02-04 14:54:58 -0300

    ✨ feat(database): add DatabaseResource module with basic structure and types

--- src/resources/database/resource.rs
+++ src/resources/database/resource.rs
@@ -5,8 +5,24 @@ use crate::http::ApiClient;
 pub struct DatabaseResource {
     http: Arc<ApiClient>,
 }
+
+pub enum DatabaseType {
+    Redis,
+    Postgres,
+    Mongo,
+}
+
 impl DatabaseResource {
     pub fn new(http: Arc<ApiClient>) -> Self {
         Self { http }
+    }
+
+    pub fn create(
+        &self,
+        name: &str,
+        memory: u32,
+        db_type: &str,
+        version: &str,
+    ) {
     }
 }
