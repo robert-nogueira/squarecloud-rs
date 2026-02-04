5663757a9d9e6ac0ef35a0c993144dbba49c5520
Robert <contact@robertnogueira.com>
2026-02-04 13:55:51 -0300

    ✨ feat(env.rs): add upsert_envs method to support bulk environment variable updates

--- src/resources/app/env.rs
+++ src/resources/app/env.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde_json::Value;
 
 use crate::{Endpoint, http::errors::ApiError, types::Env};
@@ -11,6 +13,31 @@ impl AppResource {
             .request_endpoint(Endpoint::list_app_envs(&self.id))
             .await?
             .into_result_t()?;
+
+        let map = response.as_object().unwrap();
+        let (key, value) = map.iter().next().unwrap();
+        let value_str = value
+            .as_str()
+            .map(|s| s.to_string())
+            .unwrap_or_else(|| value.to_string());
+
+        Ok(Env {
+            key: key.clone(),
+            value: value_str.to_string(),
+        })
+    }
+
+    pub async fn upsert_envs(
+        &self,
+        envs: &HashMap<String, String>,
+    ) -> Result<Env, ApiError> {
+        let endpoint = Endpoint::post_app_envs(&self.id);
+        let request = endpoint
+            .request_builder(&self.api.http_client)
+            .json(envs)
+            .build()?;
+        let response: Value =
+            self.api.execute_request(request).await?.into_result_t()?;
 
         let map = response.as_object().unwrap();
         let (key, value) = map.iter().next().unwrap();
