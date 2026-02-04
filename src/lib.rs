a4781c3452225dcfe7786316508417547d795bd0
Robert <contact@robertnogueira.com>
2026-02-04 09:36:44 -0300

    🧹 chore(lib.rs): remove commented-out resource module exports for cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,6 @@ mod settings;
 pub mod types;
 
 pub use http::endpoints::Endpoint;
-// pub use resources::database::*;
-// pub use resources::file::*;
-// pub use resources::snapshot::*;
-// pub use resources::workspace::*;
 
 #[cfg(test)]
 mod tests {
