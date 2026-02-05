b2d2c6dca5bf126c79ed53eabcdef68565d5dd08
Robert <contact@robertnogueira.com>
2026-02-05 14:39:20 -0300

    ✨ feat(database): add restore_snapshot method to DatabaseResource for restoring snapshots

--- src/resources/database/snapshots.rs
+++ src/resources/database/snapshots.rs
@@ -1,3 +1,5 @@
+use serde_json::json;
+
 use crate::{
     Endpoint,
     http::errors::ApiError,
@@ -21,5 +23,21 @@ impl DatabaseResource {
             .request_endpoint(Endpoint::create_database_snapshot(&self.id))
             .await?
             .into_result_t()
+    }
+
+    pub async fn restore_snapshot(
+        &self,
+        snapshot_id: String,
+        version_id: String,
+    ) -> Result<bool, ApiError> {
+        let endpoint = Endpoint::restore_database_snapshot(&self.id);
+        let request = endpoint
+            .request_builder(&self.client.http_client)
+            .json(&json!({"snapshotId": snapshot_id, "versionId": version_id}))
+            .build()?;
+        self.client
+            .execute_request::<()>(request)
+            .await?
+            .into_bool_result()
     }
 }
