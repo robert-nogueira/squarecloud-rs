29c442979b941e4b798b6603f71e43e951cc2560
Robert <contact@robertnogueira.com>
2026-02-05 10:40:32 -0300

    ♻️ refactor(types): consolidate database metrics and status types into database.rs for simplification and remove redundant modules

--- src/types/database.rs
+++ src/types/database.rs
@@ -1,8 +1,48 @@
 use std::sync::Arc;
 
+use chrono::{DateTime, Utc, serde::ts_milliseconds};
 use serde::{Deserialize, Serialize};
 
 use crate::{http::ApiClient, resources::DatabaseResource};
+
+#[derive(Serialize, Deserialize)]
+pub struct DatabaseNetwork {
+    pub total: String,
+    pub now: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DatabaseResumedStatus {
+    pub id: String,
+    pub running: bool,
+    pub cpu: String,
+    pub ram: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DatabaseStatus {
+    pub cpu: String,
+    pub ram: String,
+    pub status: String,
+    pub network: DatabaseNetwork,
+    #[serde(with = "ts_milliseconds")]
+    pub uptime: DateTime<Utc>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DatabaseMetrics {
+    pub date: DateTime<Utc>,
+    pub cpu: f32,
+    pub ram: f32,
+    pub net: [u32; 2],
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum DatabaseType {
+    Redis,
+    Postgres,
+    Mongo,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Database {
