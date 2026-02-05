ed43c280095505edf8d0143b243e6cb669a14aa6
Robert <contact@robertnogueira.com>
2026-02-05 16:38:07 -0300

    ✨ feat(workspace): add async delete method to WorkspaceResource for workspace removal

--- src/resources/workspace/resource.rs
+++ src/resources/workspace/resource.rs
@@ -1,6 +1,9 @@
 use std::sync::Arc;
 
-use crate::http::ApiClient;
+use crate::{
+    Endpoint,
+    http::{ApiClient, errors::ApiError},
+};
 
 pub struct WorkspaceResource {
     pub client: Arc<ApiClient>,
@@ -13,5 +16,12 @@ impl WorkspaceResource {
             client: http,
             id: id.to_string(),
         }
+    }
+
+    pub async fn delete(&self) -> Result<bool, ApiError> {
+        self.client
+            .request_endpoint::<()>(Endpoint::delete_workspace())
+            .await?
+            .into_bool_result()
     }
 }
